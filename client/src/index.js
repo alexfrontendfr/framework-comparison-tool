9cef2d2cb70563593a25aab63e73d5ecc7a127ae
alexdevbyor <contactalexfr@gmail.com>
2024-09-06 01:36:06 +0200

    Enable source maps for debugging v2

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 // src/index.js
 import React from "react";
+import BrowserRouter from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
